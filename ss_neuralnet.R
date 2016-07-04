c669653fe557118bea08e69e3f8e698a36416385
Gagandeep Singh Nagpal <gagannagpal131@gmail.com>
2016-07-04 18:04:31 +0530

    Update ss_neuralnet.R

--- ss_neuralnet.R
+++ ss_neuralnet.R
@@ -16,9 +16,11 @@ test <-  scaled[-index,]
 
 library("neuralnet")
 myFormula <- Reaction ~ Days + Subject 
+print("fitting of the neural network")
 nn <- neuralnet(formula = myFormula ,data = train,hidden = c(5,3))
 plot(nn)
 
+print("predictive analysis of the test datase")
 pr_nn <- compute(nn,test[,2:3])
 
 nn_output <- pr_nn$net.result *(max(df$Reaction)- min(df$Reaction)) + min(df$Reaction) 
@@ -28,10 +30,11 @@ finalOutput <- cbind(nn_output,test_output)
 colnames(finalOutput) <- cbind("predicted","actual")
 View(finalOutput)
 
+print("graph to show the variation between actual values and predicted values")
 plot(nn_output, type = "b", col = "blue",ylim = c(200,500),ylab ="Reaction time in ms",main = "Actual values (black) vs predicted values (blue)")
 lines(test_output,type = "b")
 
-
+print("mean absolute percentage error")
 mape_nn = (mean(abs(test_output - nn_output)/test_output))*100
 
 print(mape_nn)
